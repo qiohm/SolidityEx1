e0096118146d95d144c683dc1fb87cd8ae80b676
Gé Pé <aiohm@yahoo.fr>
2020-10-14 18:07:11 +0200

    add NatSpec to contract

--- Calc.sol
+++ Calc.sol
@@ -1,26 +1,28 @@
-pragma solidity >=0.4.0 <0.6.0;
+pragma solidity 0.6.0;
 
 /** @title calculator. */
 contract Calculator {
     
+        address private owner;
+
 
  constructor() public {
         owner = msg.sender;
     }
 
-    function add(uint w, uint h) public pure returns (uint s) {
+    function add(uint w, uint h) public view returns (uint s) {
          require(owner == msg.sender, 'Only owner can use this function.');
         s = w + h;
         }
-    function sub(uint x, uint y) public pure returns (uint s) {
+    function sub(uint x, uint y) public view returns (uint s) {
          require(owner == msg.sender, 'Only owner can use this function.');
         s = x - y;
     }
-    function mul(uint g, uint j) public pure returns (uint s) {
+    function mul(uint g, uint j) public view returns (uint s) {
          require(owner == msg.sender, 'Only owner can use this function.');
         s = g * j;
     }
-    function div(uint r, uint t) public pure returns (uint s) {
+    function div(uint r, uint t) public view returns (uint s) {
          require(owner == msg.sender, 'Only owner can use this function.');
         s = r / t;
     }
